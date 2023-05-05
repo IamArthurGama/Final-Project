3bc89274d0b2470a1e07a4a556360d441f508e12
Arthur <arthurgamasoares@gmail.com>
2023-05-05 20:40:39 -0300

    fix list, update editar

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -3,6 +3,7 @@ const db = require("../models");
 const Modelo = db.Usuario;
 const Op = db.Sequelize.Op;
 const md5 = require('md5');
+const { response } = require("express");
 
 const responseModel = {}
 function criarResponse(){
@@ -40,17 +41,20 @@ function vefPreenchimentoEditar(req, res) {
     }
 }
 
-function list(req,res){
+function list(req, res) {
     criarResponse();
     Modelo.findAll().then(data => {
-        if(data.length>0){
+        if (data.length > 0) {
             responseModel.success = true;
             responseModel.data = JSON.parse(JSON.stringify(data));
-        }else{
+            responseModel.titulo = "Lista de Usuários"
+            console.log(responseModel)
+            return res.render("admin/usuario/lista", { response: responseModel });
+        } else {
             responseModel.error = "Tabela Vazia";
+            return res.json(responseModel);
         }
-        res.render("./admin/usuario/lista",{responseModel})
-        
+
     }).catch(error => {
         responseModel.error = error;
         return res.json(responseModel);
@@ -102,29 +106,33 @@ function telaRemove(req, res) {
 
 function update(req,res){
     criarResponse();
-    modelo = vefPreenchimento(req,res);
+    modelo = vefPreenchimentoEditar(req,res);
     if(modelo){
         const id = req.params.id;
         Modelo.findByPk(id).then(data => {
-            if(data){
-                Modelo.update(modelo,{
+            if (data) {
+                Modelo.update(modelo, {
                     where: {
                         id: id
                     }
                 }).then(() => {
-                    return findById(req,res);
+                    req.flash("success_msg", "Usuário editado com sucesso!")
+                    res.redirect("/usuario")
                 }).catch(error => {
                     responseModel.error = error;
-                    return res.json(responseModel);
+                    req.flash("error_msg", "Nenhuma informação foi encontrada!")
+                    res.redirect("/usuario")
                 });
-            }else{
-                responseModel.error = "Nenhuma informação foi encontada!";
-                return res.json(responseModel);
+            } else {
+                responseModel.error = "Nenhuma informação foi encontrada!";
+                req.flash("error_msg", "Nenhuma informação foi encontrada!")
+                res.redirect("/usuario")
             }
             
         }).catch(error => {
             responseModel.error = error;
-            return res.json(responseModel);
+            req.flash("error_msg", "Erro na edição")
+            res.redirect("/usuario")
         });
     }
 }
@@ -133,7 +141,7 @@ function remove(req,res){
     criarResponse();
     const id = req.params.id;
     Modelo.findByPk(id).then(data => {
-        if(data){
+        if (data) {
             Modelo.destroy({
                 where: {
                     id: id
@@ -141,20 +149,24 @@ function remove(req,res){
             }).then(() => {
                 responseModel.success = true;
                 responseModel.data = "Usuário deletado com sucesso!";
-                return res.json(responseModel);
+                req.flash("success_msg", "Usuario deletado com sucesso!")
+                res.redirect("/usuario")
             }).catch(error => {
                 responseModel.error = error;
-                return res.json(responseModel);
+                req.flash("error_msg", "Nenhuma informação foi encontrada.")
+                res.redirect("/usuario")
             });
-        }else{
-            responseModel.error = "Nenhuma informação foi encontada!";
-            return res.json(responseModel);
+        } else {
+            responseModel.error = "Nenhuma informação foi encontrada!";
+            req.flash("error_msg", "Nenhuma informação foi encontrada.")
+            res.redirect("/usuario")
         }
-        
+
     }).catch(error => {
         responseModel.error = error;
-        return res.json(responseModel);
-    });    
+        req.flash("error_msg", "Nenhuma informação foi encontrada.")
+        res.redirect("/usuario")
+    }); 
 }
 
 function telaAdd(req, res) {
@@ -171,11 +183,13 @@ function add(req,res){
         Modelo.create(modelo).then(data => {
             responseModel.success = true;
             responseModel.data = data;
+            req.flash("success_msg", "Usuário cadastrado com sucesso!")
             res.redirect("/usuario/login")
             //return res.json(responseModel);
         }).catch(error => {
             responseModel.error = error;
-            return res.json(responseModel);
+            req.flash("error_msg", "Ocorreu um erro durante o cadastro.")
+            res.redirect("/usuario/add")
         });
     }
 }
