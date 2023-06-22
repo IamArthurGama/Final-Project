a9ba0ad71128a49f2c5ea1f2e1db8a49569f7055
Arthur <arthurgamasoares@gmail.com>
2023-06-22 20:34:11 -0300

    insert para lanches

--- DB/FPDB.FINAL.sql
+++ DB/FPDB.FINAL.sql
@@ -9,6 +9,26 @@ insert into usuarios(id, email, nome, telefone, endereco, senha, ativoAdm, ativo
 ('1', 'a@a', 'nome', '11111111111', 'xxxx', '0cc175b9c0f1b6a831c399e269772661', '1', '1',"2023-06-01 21:18:58","2023-06-01 21:18:58");
 
 
+
+
+
+
+insert into cardapios(id, nome, descricao, foto, fotoDestaque, preco, ativo, createdAt, updatedAt) values 
+('1', 'X-tudo', 'Pão, carne, queijo, bacon, salada, ovo.', '/images/1.jpg', '/images/1.jpg', '29.99', '1',"2023-06-01 21:18:58","2023-06-01 21:18:58");
+
+insert into cardapios(id, nome, descricao, foto, fotoDestaque, preco, ativo, createdAt, updatedAt) values 
+('2', 'X-Salada', 'Pão, carne, queijo, bacon, salada.', '/images/2.jpg', '/images/2.jpg', '24.99', '1',"2023-06-01 21:18:58","2023-06-01 21:18:58");
+
+insert into cardapios(id, nome, descricao, foto, fotoDestaque, preco, ativo, createdAt, updatedAt) values 
+('3', 'X-Bacon', 'Pão, carne, queijo, bacon.', '/images/3.jpg', '/images/3.jpg', '21.99', '1',"2023-06-01 21:18:58","2023-06-01 21:18:58");
+
+insert into cardapios(id, nome, descricao, foto, fotoDestaque, preco, ativo, createdAt, updatedAt) values 
+('4', 'X-Egg', 'Pão, carne, queijo e ovo.', '/images/4.jpg', '/images/4.jpg', '21.99', '1',"2023-06-01 21:18:58","2023-06-01 21:18:58");
+
+insert into cardapios(id, nome, descricao, foto, fotoDestaque, preco, ativo, createdAt, updatedAt) values 
+('5', 'Tradicional', 'Pão, carne, queijo e salada.', '/images/5.jpg', '/images/5.jpg', '19.99', '1',"2023-06-01 21:18:58","2023-06-01 21:18:58");
+
+
 update usuarios set ativoAdm = 1 where id=1;
 update usuarios set ativoAdm = 2 where id=2;
 update usuarios set ativoAdm = 0 where id=2;
